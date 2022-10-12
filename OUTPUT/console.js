797205e9ec7e78755917a4034da69e25f40df256
Lea Varžić <varzic.lea@gmail.com>
2022-10-12 11:09:20 +0200

    Output

---
+++ OUTPUT/console.js
@@ -0,0 +1,3 @@
+console.log("Hello")
+console.log("Hi!")
+console.log("The console is part of the web browser and allows you to log messages, run JavaScript code, and see errors and warnings.")
---
+++ OUTPUT/zadatak1.js
@@ -0,0 +1,4 @@
+// Write a program to print "JS is fun".
+// Note that the sentence starts with capital letters.
+
+console.log("JS is fun");
