a6e2a8596d1f2162a0ef42c4fff9cae7202f40c1
Lea Varžić <varzic.lea@gmail.com>
2022-10-12 11:22:32 +0200

    Variables

---
+++ VARIABLES/var.js
@@ -0,0 +1,20 @@
+var x = 100;
+console.log(x);
+
+x = 23;
+console.log(x); // ispisuje sve x-eve i obavezan je iza svakog nabrajanja
+
+x = 13;
+console.log(x);
+
+x = 37;
+console.log(x);
+
+x = 28;
+console.log(x);
+
+x = 73;
+console.log(x);
+
+x = 158;
+console.log(x);
