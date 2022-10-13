61529db24d06f26d9f82f5596dd27359f5a742f6
Lea Varžić <varzic.lea@gmail.com>
2022-10-13 11:35:20 +0200

    Math-operators

---
+++ BASIC_CONCEPTS/MATH_OPERATORS/operators.js
@@ -0,0 +1,31 @@
+var x = 10 + 5;
+console.log(x)
+
+var a = 10;
+var b = a + 5 + 22 + 45 + 6548;
+console.log(b)
+
+var c = 10 * 5;
+console.log(c)
+
+var d = 100 / 5;
+console.log(d)
+
+var e = 26 % 6;
+console.log(e)
+
+var f = 0, g = 10;
+var f = g++;
+console.log(f)
+
+var h = 0, i = 10;
+var h = ++i;
+console.log(h)
+
+var j = 0, k = 10;
+var j = k--;
+console.log(j)
+
+var l = 0, lj = 10;
+var l = --lj;
+console.log(l)
