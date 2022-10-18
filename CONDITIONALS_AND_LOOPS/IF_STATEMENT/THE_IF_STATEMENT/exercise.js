f97006101d0f085e8e07aa0e3eb58ff549015069
Lea Varžić <varzic.lea@gmail.com>
2022-10-18 15:50:51 +0200

    The if Statement exercise

---
+++ CONDITIONALS_AND_LOOPS/IF_STATEMENT/THE_IF_STATEMENT/exercise.js
@@ -0,0 +1,18 @@
+/* ou are planning a vacation in August.
+You are given a program that takes the month as input.
+
+Task
+Complete the code to output "vacation", if the given month is August. Don't output anything otherwise.
+
+Sample Input
+August
+
+Sample Output
+vacation
+*/
+
+var month = readLine()
+//your code goes here
+if (month == "August") {
+  console.log("vacation")
+}
