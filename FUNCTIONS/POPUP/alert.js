cd24bda64f211addb6d64ce790d5c21604066036
Lea45 <varzic.lea@gmail.com>
2022-10-26 11:55:44 +0200

    Popup boxes

---
+++ FUNCTIONS/POPUP/alert.js
@@ -0,0 +1,3 @@
+alert("Do you really want to leave this page?");
+
+alert("Hello\nHow are you?");
---
+++ FUNCTIONS/POPUP/prompt.js
@@ -0,0 +1,5 @@
+var user = prompt("Please enter your name");
+alert(user);
+
+var name = prompt("Enter your name");
+alert(name);
