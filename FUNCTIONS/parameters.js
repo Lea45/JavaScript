e4fbf8af918e877a0faa52888417de00cdda73f5
Lea45 <varzic.lea@gmail.com>
2022-10-26 10:12:38 +0200

    Function Parameters

---
+++ FUNCTIONS/parameters.js
@@ -0,0 +1,34 @@
+/* 
+functionName(param1, aram2, param3){
+    some code
+}
+*/
+
+function sayHello(name){
+    console.log("Hello, " + name); // alert
+}
+
+sayHello("Lea");
+sayHello("Ivan");
+sayHello("Adrijana");
+
+
+function myAlert(txt){
+    console.log("Yo, " + txt);
+}
+
+myAlert("Test");
+
+
+/* 
+Sometimes it’s very useful to set reminder to help you accomplish all of your tasks.
+Make the function-reminder to take that event as argument and output the corresponding message.
+*/
+function main() {
+    var eventExample = readLine();
+    setReminder(eventExample)
+}
+
+function setReminder(event) {
+    console.log('You set a reminder about ' + event);
+};
