560b224333304f71e3a8c9bded2625c60c586468
Lea45 <varzic.lea@gmail.com>
2022-10-26 10:33:43 +0200

    The return statement

---
+++ FUNCTIONS/return.js
@@ -0,0 +1,17 @@
+function myFunction(a, b){
+    return a + b;
+}
+
+var x = myFunction(5, 6);
+
+function addNumbers(a, b){
+    var c = a + b;
+    return c;
+}
+console.log(addNumbers(40, 2));
+
+function substrNumbrs(first, second){
+    var result = first - second;
+    return result;
+}
+console.log(substrNumbrs(10, 5));
