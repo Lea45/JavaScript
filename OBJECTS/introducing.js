4a9c8bd87f94c2c854895807135e160674dfda72
Lea45 <varzic.lea@gmail.com>
2022-10-27 12:01:33 +0200

    Introducing objects

---
+++ OBJECTS/introducing.js
@@ -0,0 +1,22 @@
+var person = {
+    name: "John", age: 31,
+    favColor: "green", height: 183
+};
+
+/* 
+objectName.propertyName
+or
+objectName['propertyName']
+*/
+
+var person = {
+    name: "John", age: 31,
+    favColor: "green", height: 183
+};
+var x = person.age;
+var y = person['age'];
+
+
+
+var course = {name: "JS", lessons: 41};
+console.log(course.name.length);
