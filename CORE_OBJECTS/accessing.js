17b282e6c11828549c8b41aad36b86cdf26e2f7a
Lea45 <varzic.lea@gmail.com>
2022-10-31 18:01:21 +0100

    Arrays

---
+++ CORE_OBJECTS/accessing.js
@@ -0,0 +1,7 @@
+var courses = new Array("HTML", "CSS", "JS");
+var course = courses[0]; // HTML
+courses[1] = "C++"; //Changes the second element
+
+
+var arr = new Array(3, 6, 8);
+console.log(arr[1]);
