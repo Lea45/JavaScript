7dfbd274212bf4098456d60d7a25efd8a5546254
Lea45 <varzic.lea@gmail.com>
2022-11-17 22:22:45 +0100

    The Date Object

---
+++ CORE_OBJECTS/date.js
@@ -0,0 +1,17 @@
+// call the function calc() every 2 seconds
+// setInterval(calc, 2000);
+
+
+var d = new Date(); // d stores the current date and time
+
+//new Date(miliseconds)
+//Fri Jan 02 1970 00:00:00
+var d1 = new Date(84600000);
+
+//new Date(dateString)
+//Fri Jan 02 2015 10:42:00
+var d2 = new Date("Januarz 2, 2015 10:42:00");
+
+//new Date(zear, month, day, hours, minutes, seconds, miliseconds)
+//Sat Jan 11 1988 11:42:00
+var d3 = new Date(88, 5, 11, 11, 42, 0, 0);
