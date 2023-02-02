8159d3fd0f12f2e2855993d73b4bb3cb9307e754
Lea45 <varzic.lea@gmail.com>
2023-02-02 12:36:03 +0100

    Selecting elemenmts

---
+++ DOM_EVENTS/selecting_elements.js
@@ -0,0 +1,8 @@
+// Select the element with id="text" and change its content to "Hi"
+var ob = document.getElementById("text");
+ob.innerHTML = "Hi";
+
+
+// Select all div elements and alert the content of the third div
+var arr = documentgetElementByTagName("div");
+alert(arr[2].innerHTML);
